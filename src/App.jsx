3e9cc7d811658d5f0f4191fc0395e3d21e017fbb
unknown <gausulislam5@gmail.com>
2023-10-14 23:37:29 +0600

    update section

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,283 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { FiCalendar } from 'react-icons/fi';
 
-function App() {
-  const rows = [];
-  for (let i = 1; i <= 7; i++) {
-    const columns = [];
-    for (let j = 1; j <= 10; j++) {
-      columns.push(<td key={j}>Row {i}, Col {j}</td>);
+const App = () => {
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const [monthDifference, setMonthDifference] = useState(null);
+  const [excludedDates, setExcludedDates] = useState([]);
+  const [numberOfDays, setNumberOfDays] = useState(null);
+  const [numberOfLeads, setNumberOfLeads] = useState(0);
+  const [expectedLeadCount, setExpectedLeadCount] = useState(null);
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [savedData, setSavedData] = useState([]); // New state for saved data
+  const [lastUpdated, setLastUpdated] = useState(null); // New state for last update timestamp
+
+  const handleStartDateChange = (date) => {
+    setStartDate(date);
+    calculateMonthDifference(date, endDate);
+    calculateNumberOfDays(date, endDate, excludedDates);
+    calculateExpectedLeadCount(numberOfLeads, numberOfDays);
+  };
+
+  const handleEndDateChange = (date) => {
+    setEndDate(date);
+    calculateMonthDifference(startDate, date);
+    calculateNumberOfDays(startDate, date, excludedDates);
+    calculateExpectedLeadCount(numberOfLeads, numberOfDays);
+  };
+
+  const calculateMonthDifference = (start, end) => {
+    if (start && end) {
+      const diffInMonths =
+        (end.getFullYear() - start.getFullYear()) * 12 +
+        (end.getMonth() - start.getMonth());
+      setMonthDifference(diffInMonths);
+    } else {
+      setMonthDifference(null);
     }
-    rows.push(<tr key={i}>{columns}</tr>);
-  }
+  };
+
+  const calculateNumberOfDays = (start, end, excluded) => {
+    if (start && end) {
+      let days = 0;
+      for (let currentDate = new Date(start); currentDate <= end; currentDate.setDate(currentDate.getDate() + 1)) {
+        const currentDateString = currentDate.toISOString().split('T')[0];
+        if (!excluded.includes(currentDateString)) {
+          days++;
+        }
+      }
+      setNumberOfDays(days);
+    } else {
+      setNumberOfDays(null);
+    }
+  };
+
+  const calculateExpectedLeadCount = (leads, days) => {
+    if (leads > 0 && days > 0) {
+      const expectedCount = leads / days;
+      setExpectedLeadCount(expectedCount);
+    } else {
+      setExpectedLeadCount(null);
+    }
+  };
+
+  const handleExcludeDate = (date) => {
+    setExcludedDates([...excludedDates, date]);
+  };
+
+  const handleSave = () => {
+    // Create a new data entry
+    const savedEntry = {
+      startDate,
+      endDate,
+      monthDifference,
+      excludedDates,
+      numberOfDays,
+      numberOfLeads,
+      expectedLeadCount,
+      lastUpdated: new Date().toLocaleString(), // Set the last update timestamp
+    };
+
+    // Add the entry to the list of saved data
+    setSavedData([...savedData, savedEntry]);
+
+    // Reset the form and clear the flag indicating form submission
+    setStartDate(null);
+    setEndDate(null);
+    setMonthDifference(null);
+    setExcludedDates([]);
+    setNumberOfDays(null);
+    setNumberOfLeads(0);
+    setExpectedLeadCount(null);
+    setIsFormSubmitted(true);
+
+    // Update the last update timestamp
+    setLastUpdated(savedEntry.lastUpdated);
+  };
+  const handleCancel = () => {
+    // Reset the form and clear the flag indicating form submission
+    setStartDate(null);
+    setEndDate(null);
+    setMonthDifference(null);
+    setExcludedDates([]);
+    setNumberOfDays(null);
+    setNumberOfLeads(0);
+    setExpectedLeadCount(null);
+    setIsFormSubmitted(false);
+  };
 
   return (
-    <div className='table-container'>
-     <p className='add-new'>Add New</p>
-      <table className="custom-table">
+    <div>
+      <h1>Simple HTML Table</h1>
+      <table>
         <thead>
           <tr>
             <th>Action</th>
             <th>ID</th>
             <th>Start Date</th>
             <th>End Date</th>
-            <th>Month, Year</th>
-            <th>Dates ExCluded</th>
+            <th>Month Difference</th>
+            <th>Dates Excluded</th>
             <th>Number of Days</th>
             <th>Lead Count</th>
             <th>Expected DRR</th>
             <th>Last Updated</th>
           </tr>
         </thead>
-        <tbody>{rows}</tbody>
-      </table>
-    </div>
-  )
-}
+        <tbody>
+          <tr>
+            <td>
+              <input
+                type="text"
+                className="block w-20 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50"
+              />
+            </td>
+            <td>
+              <input
+                type="text"
+                className="block w-20 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50"
+              />
+            </td>
+            <td>
+              <div className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50">
+                <FiCalendar className="date-picker-icon" />
+                <DatePicker
+                  selected={startDate}
+                  onChange={handleStartDateChange}
+                  dateFormat="yyyy-MM-dd"
+                  placeholderText="Start Date"
+                  isClearable
+                  excludeDates={excludedDates}
+                />
+              </div>
+            </td>
+            <td>
+              <div className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50">
+                <FiCalendar className="date-picker-icon" />
+                <DatePicker
+                  selected={endDate}
+                  onChange={handleEndDateChange}
+                  dateFormat="yyyy-MM-dd"
+                  placeholderText="End Date"
+                  isClearable
+                  excludeDates={excludedDates}
+                />
+              </div>
+            </td>
+            <td>
+              <input
+                type="text"
+                className="block w-20 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50"
+                value={monthDifference !== null ? monthDifference : ''}
+                readOnly
+              />
+            </td>
+            <td className='datePicker'>
+              <FiCalendar className="date-picker-icon" />
+              <DatePicker
+                selected={null}
+                onChange={handleExcludeDate}
+                excludeDates={excludedDates}
+                dateFormat="yyyy-MM-dd"
+                placeholderText="Exclude Date"
+                isClearable
+                highlightDates={(date) =>
+                  excludedDates.some((excludedDate) =>
+                    date.toISOString().includes(excludedDate.toISOString())
+                  )
+                }
+              />
+            </td>
+            <td>
+              <input
+                type="text"
+                className="block w-20 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50"
+                value={numberOfDays !== null ? numberOfDays : ''}
+                readOnly
+              />
+            </td>
+            <td>
+              <input
+                type="number"
+                className="block w-20 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50"
+                value={numberOfLeads}
+                onChange={(e) => {
+                  setNumberOfLeads(e.target.value);
+                  calculateExpectedLeadCount(e.target.value, numberOfDays);
+                }}
+              />
+            </td>
+            <td>
+              {expectedLeadCount !== null ? (
+                <input
+                  type="text"
+                  className="block w-20 p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50"
+                  value={expectedLeadCount.toFixed(2)}
+                  readOnly
+                />
+              ) : (
+                <span>N/A</span>
+              )}
+            </td>
 
-export default App
+            <td>
+              <button
+                onClick={handleSave}
+                className='bg-blue-500 px-5 py-2 rounded-md hover:bg-blue-700 text-white font-bold'
+              >
+                Save
+              </button>
+              <br />
+              <button
+                onClick={handleCancel}
+                className='bg-red-500 px-5 py-2 rounded-md hover-bg-red-700 text-white font-bold mt-2'
+              >
+                Cancel
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      {isFormSubmitted && (
+        <p>Data submitted successfully. You can reset the form or redirect the user as needed.</p>
+      )}
+      {/* Display the saved data */}
+      <div>
+        <h2>Saved Data</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Start Date</th>
+              <th>End Date</th>
+              <th>Month Difference</th>
+              <th>Dates Excluded</th>
+              <th>Number of Days</th>
+              <th>Lead Count</th>
+              <th>Expected DRR</th>
+            </tr>
+          </thead>
+          <tbody>
+            {savedData.map((entry, index) => (
+              <tr key={index}>
+                <td>{entry.startDate}</td>
+                <td>{entry.endDate}</td>
+                <td>{entry.monthDifference}</td>
+                <td>{entry.excludedDates.join(', ')}</td>
+                <td>{entry.numberOfDays}</td>
+                <td>{entry.numberOfLeads}</td>
+                <td>{entry.expectedLeadCount.toFixed(2)}</td>
+                <td>{entry.lastUpdated}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default App;
